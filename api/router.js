c85a45c8cfbc3cb42f6d36df7a4a8ea493110954
Juan Carlos Asensio <jasensio@ufm.edu>
2023-06-26 17:12:26 -0400

    Remove unused HN code

--- api/router.js
+++ api/router.js
@@ -3,15 +3,9 @@ const {
   getPlayersByName, 
   getPlayerData 
 } = require('./handlers/basketballRef');
-const { 
-  getArticlesByQuery, 
-  getFrontPageArticles,
-} = require('./handlers/hackerNews');
 
 const router = Router();
 
-router.get('/hn', getFrontPageArticles)
-router.get('/hn/:query', getArticlesByQuery)
 router.get('/players/:name', getPlayersByName)
 router.get('/players', getPlayersByName)
 router.get('/player', getPlayerData)
